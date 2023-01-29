ec40253369fc6c04c493ff25244e627a1bcf927f
whatwewant <tobewhatwewant@outlook.com>
2023-01-29 23:08:24 +0800

    fix: run in MacOS

--- src/action.ts
+++ src/action.ts
@@ -16,9 +16,19 @@ export function getActionInput(): IActionInput {
 }
 
 export async function install(_version?: string) {
-  await $`sudo apt update -y`;
-
-  await $`sudo apt install -y curl`;
+  const platform = await $`uname -s`;
+  switch (platform) {
+    case 'Darwin':
+      await $`brew update`;
+      await $`brew install -y curl`;
+      break;
+    case 'Linux':
+      await $`sudo apt update -y`;
+      await $`sudo apt install -y curl`;
+      break;
+    default:
+      throw new Error(`unsupport platform: ${platform}`);
+  }
 
   await $`curl -o- https://raw.githubusercontent.com/zcorky/zmicro/master/install | bash`;
 
