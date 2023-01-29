d003e81aa3936c34d9549eba9659ef904567833f
whatwewant <tobewhatwewant@outlook.com>
2023-01-29 22:39:01 +0800

    :tada:

---
+++ __tests__/sendMessage.spec.ts
@@ -0,0 +1,5 @@
+describe('@zcorky/action-setup-zmicro', () => {
+  it('ok', async () => {
+    expect(1).toEqual(1);    
+  });
+});
---
+++ src/action.ts
@@ -0,0 +1,34 @@
+import * as core from '@actions/core';
+import { api } from '@cliz/cli';
+
+api.$.verbose = true;
+
+export interface IActionInput {
+  version?: string;
+}
+
+export function getActionInput(): IActionInput {
+  const version: string = core.getInput('url');
+
+  return {
+    version,
+  };
+}
+
+export async function install(_version?: string) {
+  await api.$`apt update -y`;
+
+  await api.$`apt install -y curl`;
+
+  await api.$`curl -o- https://raw.githubusercontent.com/zcorky/zmicro/master/install | bash`;
+}
+
+export async function action() {
+  const actionInput = getActionInput();
+
+  await install(actionInput.version);
+}
+
+action().catch((error) => {
+  core.setFailed(error.message);
+});
