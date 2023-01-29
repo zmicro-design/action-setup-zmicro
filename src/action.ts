c0a1b94e325d88ad1b1de4270c69f936bd6f7f6f
whatwewant <tobewhatwewant@outlook.com>
2023-01-29 23:20:17 +0800

    chore: use action core.startGroup

--- src/action.ts
+++ src/action.ts
@@ -19,22 +19,29 @@ export async function install(_version?: string) {
   const platform = await $`uname -s`;
   switch (platform) {
     case 'Darwin':
+      core.startGroup(`Download curl with brew ...`);
       await $`brew update`;
       await $`brew install curl`;
+      core.endGroup();
       break;
     case 'Linux':
+      core.startGroup(`Download curl with apt ...`);
       await $`sudo apt update -y`;
       await $`sudo apt install -y curl`;
+      core.endGroup();
       break;
     default:
       throw new Error(`unsupport platform: ${platform}`);
   }
 
+  core.startGroup(`Install zmicro ...`);
   await $`curl -o- https://raw.githubusercontent.com/zcorky/zmicro/master/install | bash`;
+  core.endGroup();
 
+  core.startGroup(`Show zmicro info and version ...`);
   await $`zmicro info`;
-
   await $`zmicro -v`;
+  core.endGroup();
 }
 
 export async function action() {
